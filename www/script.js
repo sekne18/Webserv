fd7a5be03fb4f21406c9cdef319e911e0217a1d3
Jan Sekne <jsekne@a1-r1-p9.s19.be>
2025-03-06 19:09:41 +0100

    modified .js file

--- www/script.js
+++ www/script.js
@@ -8,8 +8,8 @@ async function sendRequest(method) {
   }
   try {
     let response = await fetch(url, options);
-    let result = await response.json();
-    document.getElementById(method.toLowerCase() + 'Response').innerText = JSON.stringify(result, null, 1);
+    let result = await response.text();
+    document.getElementById(method.toLowerCase() + 'Response').innerText = result;
   } catch (error) {
     document.getElementById(method.toLowerCase() + 'Response').innerText = 'Error: ' + error;
   }
